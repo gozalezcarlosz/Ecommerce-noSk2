4bd040e1e7bc6044988112a8e644480e5fbd4314
gozalezcarlosz <gozalescarlos907@gmail.com>
2026-02-05 01:19:35 -0500

    Controlador Usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,33 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        return view('admin.usersview', compact('users'))->with('title', __('Users'));
+        $users = User::paginate(10);
+        //return view('admin.users.index', compact('users'))->with('title', __('Users'));
+        return view('admin.users.example', compact('users'))->with('title', __('Users'));
+    }
+
+    public function show(int $id){
+        $user = User::findOrFail($id);
+        return view('admin.users.show', compact('user'))->with('title', __('User Details'));
+    }
+
+    public function edit(int $id){
+        $user = User::findOrFail($id);
+        return view('admin.users.edit', compact('user'))->with('title', __('Edit User'));
+    }
+
+    public function destroy(int $id){
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->route('admin.users')->with('tipo_alerta', 'success')->with('message', __('User deleted successfully'));
+    }
+
+    public function create(){
+        $user = new User();
+        $user->name = request('name');
+        $user->email = request('email');
+        $user->password = bcrypt(request('password'));
+        $user->save();
+        return redirect()->route('admin.users')->with('tipo_alerta', 'success')->with('message', __('User created successfully'));
     }
 }
